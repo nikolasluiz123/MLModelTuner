ee332510a0b17586b3c6a9a6aa9fb3003865b159
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2024-12-01 11:03:24 -0300

    feat: possibilidade de passar um LabelEncoder para o validador adicional de classificação

--- wrappers/scikit_learn/validator/additional_validator.py
+++ wrappers/scikit_learn/validator/additional_validator.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 from wrappers.common.validator.common_additional_validator import CommonClassifierAdditionalValidator, \
     CommonRegressorAdditionalValidator
@@ -16,16 +17,20 @@ class ScikitLearnClassifierAdditionalValidator(CommonClassifierAdditionalValidat
                  validation_results_directory: str,
                  prefix_file_names: str,
                  random_state=42,
-                 show_graphics: bool = True):
+                 show_graphics: bool = True,
+                 label_encoder: LabelEncoder = None):
         """
         :param estimator: Modelo do scikit-learn que será avaliado
-        :param: random_state: Seed utilizada no split dos dados. É necessária, pois a validação adicional ocorre fora do
+        :param random_state: Seed utilizada no split dos dados. É necessária, pois a validação adicional ocorre fora do
                               ProcessManager.
+        :param label_encoder: Implementação que foi utilizada para transformar valores string categóricos em números. Nesse
+                             caso, ao realizar a validação adicional, pode ser desejado visualizar os valores string.
         """
 
         super().__init__(data, validation_results_directory, prefix_file_names, show_graphics)
         self.estimator = estimator
         self.random_state = random_state
+        self.label_encoder = label_encoder
 
     def validate(self):
         x_train, x_test, y_train, y_test = train_test_split(self.data[0],
@@ -35,6 +40,10 @@ class ScikitLearnClassifierAdditionalValidator(CommonClassifierAdditionalValidat
 
         self.estimator.fit(x_train, y_train)
         y_pred = self.estimator.predict(x_test)
+
+        if self.label_encoder is not None:
+            y_test = self.label_encoder.inverse_transform(y_test)
+            y_pred = self.label_encoder.inverse_transform(y_pred)
 
         self._show_classification_report(y_test, y_pred)
         self._show_confusion_matrix(y_test, y_pred, list(np.unique(self.data[1])))
